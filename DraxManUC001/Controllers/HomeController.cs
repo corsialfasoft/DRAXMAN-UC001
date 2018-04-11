0affc3cdf8ad75ed92c055bde4b2ecb0fcde3389
emmeacs <massimo.franzoso8895@gmail.com>
2018-04-11 12:12:56 +0200

    Aggiunto Ricerca

--- DraxManUC001/Controllers/HomeController.cs
+++ DraxManUC001/Controllers/HomeController.cs
@@ -26,5 +26,34 @@ namespace DraxManUC001.Controllers
 
 			return View();
 		}
+		public ActionResult Ricerca()
+		{
+			return View();
+		}
+		[HttpPost]
+		public ActionResult Ricerca(string id,string descrizione)
+		{
+			int codice;
+			if (id !="" && int.TryParse(id,out codice)) {
+				Prodotto prodotto = dm.Search(codice);
+				if (prodotto == null) {
+					ViewBag.Message=$"La ricerca con il seguente codice {codice} non ha prodotto alcun risultato";
+					return View();
+				}
+				ViewBag.Prodotto = prodotto;
+				return View("Dettaglio");
+			}else if (descrizione !="" ){ 
+				List<Prodotto> prodotti = dm.Search(descrizione);
+				if(prodotti == null) {
+					ViewBag.Message ="Non è stato trovato alcun prodotto che corrisponda alla descrizione";
+					return View();
+				}
+				ViewBag.Prodotti = prodotti;
+				return View("ListaProdotti");
+			} else {
+				ViewBag.Message="Inserire almeno un parametro di ricerca";
+				return View();
+			}
+		}
 	}
 }

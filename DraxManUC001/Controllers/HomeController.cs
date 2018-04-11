87be7d92b76612f0263f70b90bee18fb0908f8dc
emmeacs <massimo.franzoso8895@gmail.com>
2018-04-11 12:16:16 +0200

    Commit

--- DraxManUC001/Controllers/HomeController.cs
+++ DraxManUC001/Controllers/HomeController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DraxManUC001.Models;
 
 namespace DraxManUC001.Controllers
 {
 	public class HomeController : Controller
 	{
+		DomainModel dm=new DomainModel();
 		public ActionResult Index()
 		{
 			return View();
